664dd4436e2c06423e800b0b8589f14f7975676c
Ritwij Sinha <ritwijxsinha@gmail.com>
2018-04-05 16:59:00 +0530

    added logout and login retention

--- app/src/main/java/lango/mr/paathshala/MainActivity.java
+++ app/src/main/java/lango/mr/paathshala/MainActivity.java
@@ -1,31 +1,145 @@
 package lango.mr.paathshala;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "EmailPassword";
+    private EditText email;
+    private EditText password;
+    FirebaseAuth mAuth;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+        db = openOrCreateDatabase("logged.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS USER(EMAIL VARCHAR(30),PASSWORD VARCHAR(30));");
+
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        mAuth = FirebaseAuth.getInstance();
+
+        Cursor c = db.rawQuery("SELECT * FROM USER;", null);
+
+        try {
+            if (c.moveToFirst()) {
+                db.close();
+                Intent i = new Intent(MainActivity.this, Teacher.class);
+                startActivity(i);
+            }
+        }
+        catch (Exception e){
+            Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_SHORT).show();
+        }
+
+        findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i=new Intent(MainActivity.this,Register.class);
                 startActivity(i);
             }
         });
-        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent m=new Intent(MainActivity.this,LogIn.class);
-                startActivity(m);
+
+                signIn(email.getText().toString(), password.getText().toString());
             }
         });
+
+
+
+
+
+
     }
+
+
+    private void signIn(final String email, final String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    final String e = email;
+                    final String p = password;
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            db.execSQL("INSERT INTO USER VALUES('"+email+"','"+password+"');");
+                            Intent i=new Intent(MainActivity.this,Teacher.class);
+                            startActivity(i);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Login failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Networks Error.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        ;
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email1 = email.getText().toString();
+        if (TextUtils.isEmpty(email1)) {
+            email.setError("Required.");
+            valid = false;
+        } else {
+            email.setError(null);
+        }
+
+        String pass = password.getText().toString();
+        if (TextUtils.isEmpty(pass)) {
+            password.setError("Required.");
+            valid = false;
+        } else {
+            password.setError(null);
+        }
+        return valid;
+
+    }
+
+
 }
--- app/src/main/java/lango/mr/paathshala/Teacher.java
+++ app/src/main/java/lango/mr/paathshala/Teacher.java
@@ -1,13 +1,32 @@
 package lango.mr.paathshala;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Teacher extends AppCompatActivity {
+
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+
+
+        db = openOrCreateDatabase("logged.db", MODE_PRIVATE, null);
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                db.execSQL("DROP TABLE USER;");
+                db.close();
+                Intent i=new Intent(Teacher.this,MainActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
